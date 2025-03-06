ca2e682b36bdeec273fc7971c0e202f88d7654a2
Kami <166147703+kami2611@users.noreply.github.com>
2025-03-06 22:57:22 +0500

    added the large heading option

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const bgChangeButton = document.getElementById('backGroundChangeAlert');
 bgChangeButton.addEventListener('click', ()=>{
     alert('backGround changed alert');
 });
+const header = document.getElementById('header');
+header.addEventListener('click', ()=>{
+    header.classList.add('header-class');
+})
